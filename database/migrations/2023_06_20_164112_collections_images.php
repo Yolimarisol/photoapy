b5b2f582a6d3d0df12fc37887cfc9d2942b37020
Yolimarisol <yolamarisol@hotmail.com>
2023-06-20 15:53:29 -0600

    foreign repair

---
+++ database/migrations/2023_06_20_164112_collections_images.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections_images', function (Blueprint $table) {
+            $table->uuid('collections_id');
+            $table->index('collections_id');
+            $table->foreign('collections_id')->references('id')->on('collections')->onDelete('cascade');
+            $table->uuid('images_id');
+            $table->index('images_id');
+            $table->foreign('images_id')->references('id')->on('images')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections_images');
+    }
+};
